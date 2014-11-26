588fef2ebb9a572210403c8a1dc78a176b494358
longpd <longlv2710@gmail.com>
2014-11-26 15:08:33 +0700

    setup source code

---
+++ info.php
@@ -0,0 +1,8 @@
+<?php
+// we connect to example.com and port 3307
+$link = mysql_connect('localhost', 'babimart_magento', 'magento@45321');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
+mysql_close($link);
